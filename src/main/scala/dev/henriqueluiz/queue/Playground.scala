f4ba1e216e1f597003511e9ee921ffb4ea451205
Henrique Luiz <heenluy@gmail.com>
2023-08-02 18:23:44 -0300

    create linear queue

--- src/main/scala/dev/henriqueluiz/queue/Playground.scala
+++ src/main/scala/dev/henriqueluiz/queue/Playground.scala
@@ -15,6 +15,26 @@ package dev.henriqueluiz.queue
     Q1.enqueue(3)
     Q1.enqueue(4)
     Q1.enqueue(5)
-    println("QUEUE ONE => " + Q1.peek())
+    println("QUEUE ONE => " + Q1)
+
+    Q1.dequeue()
+    
+    // ADICIONARÁ O ITEM NO INÍCIO DA FILA
+    Q1.enqueue(1)
+    println("QUEUE ONE => " + Q1)
+
+    // QUEUE ONE
+    val Q2 = Queue2.apply(5)
+    Q2.enqueue(1)
+    Q2.enqueue(2)
+    Q2.enqueue(3)
+    Q2.enqueue(4)
+    Q2.enqueue(5)
+    println("QUEUE TWO => " + Q2)
+
+    //Q2.dequeue()
+
+    // CAUSARÁ UM ERRO DE ÍNDICE
+    //Q2.enqueue(6)
 
     
