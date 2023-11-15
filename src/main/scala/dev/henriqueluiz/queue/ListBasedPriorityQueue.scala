2ae55aefb9c1b8719bc7df6fba6ead80713a1a4b
Henrique Luiz <heenluy@gmail.com>
2023-11-14 22:12:06 -0300

    changing pc

---
+++ src/main/scala/dev/henriqueluiz/queue/ListBasedPriorityQueue.scala
@@ -0,0 +1,7 @@
+package dev.henriqueluiz.queue
+
+class ListBasedPriorityQueue {
+  // Criar uma fila prioritária baseada em uma lista linkada.
+  // Depois criar uma baseada em Heap, por fim baseada em Árvore.
+  // Conteúdo do GeekForGeeks
+}
